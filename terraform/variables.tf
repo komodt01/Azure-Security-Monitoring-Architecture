6af480a400936e2aa9b7f951baf77ff0ddb57f6e
Karl Omodt <90644248+komodt01@users.noreply.github.com>
2026-09-21 09:03:23 -0600

    Enhance variable definitions in variables.tf
    Added descriptions and types for several variables in the Terraform configuration.

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,10 +1,31 @@
 variable "location" {
-  default = "eastus"
+  description = "Azure region used for the lab resources."
+  type        = string
+  default     = "eastus"
 }
 
-variable "resource_group_name" {}
-variable "workspace_name" {}
-variable "vm_admin_username" {}
-variable "vm_admin_password" {}
-variable "ssh_allowed_ip" {}
-variable "user_object_id" {}
+variable "resource_group_name" {
+  description = "Name of the Azure resource group."
+  type        = string
+}
+
+variable "workspace_name" {
+  description = "Name of the Log Analytics Workspace."
+  type        = string
+}
+
+variable "vm_admin_username" {
+  description = "Administrative username for the Linux VM."
+  type        = string
+}
+
+variable "vm_admin_password" {
+  description = "Administrative password used by the lab VM."
+  type        = string
+  sensitive   = true
+}
+
+variable "ssh_allowed_ip" {
+  description = "Source IP address permitted to connect to the lab VM over SSH."
+  type        = string
+}
